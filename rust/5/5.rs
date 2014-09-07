3af86a6852b3c0df98b2cd84885363b25d79f2be
franc0is <francois@getpebble.com>
2014-09-06 19:28:58 -0700

    add solution to problem 5

---
+++ rust/5/5.rs
@@ -0,0 +1,22 @@
+fn print_hex(string: Vec<u8>) {
+  for byte in string.iter() {
+    print!("{:x}", byte.clone());
+  }
+  print!("\n");
+}
+
+fn main() {
+  let args: Vec<String> = std::os::args();
+  let input = args[1].as_bytes();
+  let secret = args[2].as_bytes();
+  let mut xored_bytes: Vec<u8> = Vec::new();
+
+  let mut secret_idx: uint = 0;
+  for byte in input.iter() {
+    xored_bytes.push(byte ^ secret[secret_idx]);
+    secret_idx = (secret_idx + 1) % secret.len();
+  }
+
+  print_hex(xored_bytes);
+
+}
