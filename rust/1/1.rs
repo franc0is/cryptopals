a9b9805a9d02758581b71b7a9eb0fd57bf4b61b0
franc0is <francois@getpebble.com>
2014-09-08 04:08:01 -0700

    challenge 1: reimplement using rust bas64

--- rust/1/1.rs
+++ rust/1/1.rs
@@ -1,29 +1,20 @@
-fn base64_digit_to_char(digit: u8) -> char {
-  let ascii_code: u8 = match digit {
-    digit if digit < 26 => 65 + digit,
-    digit if digit < 52 => 97 + digit - 26,
-    _ => 48 + digit - 52
-  } as u8;
-  return ascii_code as char;
-}
+extern crate serialize;
+use serialize::base64::{ToBase64, STANDARD};
 
 fn main() {
-  // read from args
   let args: Vec<String> = std::os::args();
-  let hex_string = args.get(1).as_bytes();
+  let hex_string = args[1].as_bytes();
 
-  // divide hex in chunks of 3 digits, which correspond to 2 base64 digits
-  let hex_chunks = hex_string.chunks(3);
+  let mut hex_chunks = hex_string.chunks(2);
 
-  let mut base64_digits: Vec<u8> = Vec::new();
-  for hex_chunk in hex_chunks.clone() {
+  let mut bytes: Vec<u8> = Vec::new();
+  for hex_chunk in hex_chunks {
     let num = std::int::parse_bytes(hex_chunk, 16);
-    base64_digits.push((num.unwrap() / 64) as u8);
-    base64_digits.push((num.unwrap() % 64) as u8);
+    bytes.push(num.unwrap() as u8);
   }
 
-  for digit in base64_digits.iter() {
-    print!("{}", base64_digit_to_char(digit.clone()));
-  }
-  print!("\n");
+  let base64 = bytes.as_slice().to_base64(STANDARD);
+
+  println!("{}", base64);
+
 }
