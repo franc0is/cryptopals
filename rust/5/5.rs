f2b0a587a76db0525f16bbc74377d8b72ba0f0ec
franc0is <francois@getpebble.com>
2014-10-24 10:22:06 -0700

    more idiomatic challenge 5

--- rust/5/5.rs
+++ rust/5/5.rs
@@ -7,16 +7,14 @@ fn print_hex(string: Vec<u8>) {
 
 fn main() {
   let args: Vec<String> = std::os::args();
-  let input = args[1].as_bytes();
-  let secret = args[2].as_bytes();
-  let mut xored_bytes: Vec<u8> = Vec::new();
+  let input: Vec<u8> = args[1].as_bytes().to_vec();
+  let secret: Vec<u8> = args[2].as_bytes().to_vec();
 
-  let mut secret_idx: uint = 0;
-  for byte in input.iter() {
-    xored_bytes.push(byte ^ secret[secret_idx]);
-    secret_idx = (secret_idx + 1) % secret.len();
-  }
+  let decrypted = input.iter()
+                       .enumerate()
+                       .map(|(idx, byte)| { byte ^ secret[idx % secret.len()] })
+                       .collect();
 
-  print_hex(xored_bytes);
+  print_hex(decrypted);
 
 }
