686b18e12fcfe97f0eefc2ec67df03554503a0d1
franc0is <francois@getpebble.com>
2014-10-24 17:05:37 -0700

    solve challenge 8

---
+++ rust/8/8.rs
@@ -0,0 +1,31 @@
+#![feature(globs)]
+
+extern crate serialize;
+use serialize::base64::FromBase64;
+use std::io::File;
+use std::io::BufferedReader;
+use std::collections::HashSet;
+
+fn is_aes_ecb(bytes: Vec<u8>) -> bool {
+  let len = bytes.len() / 16;
+  let blocks = bytes.as_slice().chunks(16);
+  let mut set: HashSet<&[u8]> = HashSet::new();
+  for block in blocks.clone() {
+    set.insert(block);
+  }
+
+  len != set.len()
+}
+
+fn main() {
+  let args: Vec<String> = std::os::args();
+  let path = Path::new(args[1].clone());
+  let mut file = BufferedReader::new(File::open(&path));
+  for (i, line_iter) in file.lines().enumerate() {
+    let input: Vec<u8> = match line_iter { Ok(x) => x, Err(e) => fail!(e) }.into_bytes();
+    let bytes = input.as_slice().from_base64().unwrap();
+    if is_aes_ecb(bytes) {
+      println!("Found ECB at line {}", i);
+    }
+  }
+}
